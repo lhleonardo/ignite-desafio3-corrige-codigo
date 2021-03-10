b953f189bb125e3df513db575184d2420d7981d1
Leonardo Braz <lhleonardo05@gmail.com>
2021-03-10 09:21:58 -0300

    refactor: adiciona novo repositório a lista de repositórios

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
+  repositories.push(repository);
+
   return response.json(repository);
 });
 
@@ -30,7 +32,7 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repositoryIndex = repositories.findindex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
